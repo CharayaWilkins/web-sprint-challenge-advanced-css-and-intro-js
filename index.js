f8d5fac7f08ce2431220132ec083e5b831a790f7
CharayaWilkins <rayawilkins@gmail.com>
2021-07-16 12:23:26 -0700

    Most tasks completed

--- index.js
+++ index.js
@@ -244,8 +244,7 @@ Example born in 1901 and died in 1959 - included -- born in 1889 and died in 192
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 // Hint - Look up the .split() method
 
-function get20s(array) {
-  /*Your Code Here*/
+function get20s() {
 }
 
 
@@ -259,9 +258,15 @@ Use removeArtist to do the following:
 
 For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
-}
+function removeArtist(array) {
+  for(let i = 0; i < array.length; i++){
+    if(array[i].id === 0){
+      array.splice(i, 1);
+    }
+  }
+  return array.length;
+  }
+  console.log('Task 5', removeArtist(artists));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
